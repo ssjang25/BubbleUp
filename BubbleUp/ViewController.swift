467f99a2050922382c66495fdf80a4da9bc4c84d
Sarah Jang <ssjang@umass.edu>
2023-11-11 16:36:57 -0500

    Initial Commit

---
+++ BubbleUp/ViewController.swift
@@ -0,0 +1,15 @@
+//this class can be used to add specific functionality
+//to Main.storyboard (go to HomeViewController.swift
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
