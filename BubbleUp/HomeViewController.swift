4c49d1f441331726538ccdeb36236083060b7236
Sarah Jang <ssjang@umass.edu>
2023-11-12 00:26:36 -0500

    Added map functionality to determine user's current location and pinning.

--- BubbleUp/HomeViewController.swift
+++ BubbleUp/HomeViewController.swift
@@ -2,13 +2,39 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class HomeViewController: UIViewController {
-
+class HomeViewController: UIViewController, MKMapViewDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    var locationManager = CLLocationManager()
+    let baseCoordinate = CLLocationCoordinate2DMake(44.1487225,-87.5684114)
+    
+    @IBAction func tapGesture(_ sender: UITapGestureRecognizer) {
+        if sender.state == .ended{
+            let locationInView = sender.location(in: mapView)
+            let tappedCoordinate = mapView.convert(locationInView , toCoordinateFrom: mapView)
+            addAnnotation(coordinate: tappedCoordinate)
+        }
+    }
+    
+    @IBAction func clearAnnotations(_ sender: UIButton) {
+        mapView.removeAnnotations(mapView.annotations)
+    }
+    
+    func addAnnotation(coordinate:CLLocationCoordinate2D){
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinate
+        mapView.addAnnotation(annotation)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.distanceFilter = kCLDistanceFilterNone
+        locationManager.startUpdatingLocation()
+        mapView.showsUserLocation = true
     }
     
 
--- BubbleUp/SettingViewController.swift
+++ BubbleUp/SettingViewController.swift
@@ -10,13 +10,6 @@ import UIKit
 class SettingViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        //Parse JSON
-        let decoder = JSONDecoder()
-        //get json data
-        let data = ""
-        //Will convert JSON (data) into an instance of AccountPage. If there are any errors look at AccountPage and Post
-        let accPage = try decoder.decode(AccountPage.self, from: data)
-        
     }
     
     
