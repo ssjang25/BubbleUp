393af230d05c407482af2a4e016314fe0979d7c8
Sarah Jang <ssjang@umass.edu>
2023-11-11 21:34:31 -0500

    Added code to allow json data to be added

---
+++ BubbleUp/AccountPage.swift
@@ -0,0 +1,15 @@
+//
+//  AccountPage.swift
+//  BubbleUp
+//
+//  Created by sarah jang on 11/11/23.
+//
+
+import Foundation
+
+struct AccountPage: Codable {
+    var phoneNumber: String = ""
+    var username: String = ""
+    var posts:Post?
+    
+}
---
+++ BubbleUp/SettingViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SettingViewController.swift
+//  BubbleUp
+//
+//  Created by sarah jang on 11/11/23.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //Parse JSON
+        let decoder = JSONDecoder()
+        //get json data
+        let data = ""
+        //Will convert JSON (data) into an instance of AccountPage. If there are any errors look at AccountPage and Post
+        let accPage = try decoder.decode(AccountPage.self, from: data)
+        
+    }
+    
+    
+}
